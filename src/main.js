500cb72123c14182a0605dcaa9b8064a3f7ac1a4
Sheep-Wei <11035187@fhsh.tp.edu.tw>
2022-05-06 11:58:32 +0800

    基本版型完成

--- src/main.js
+++ src/main.js
@@ -4,4 +4,20 @@ $(document).ready(function () {
   $("#sign-in-form").submit(function (e) {
     e.preventDefault();
   });
+
+  $("#account").blur(function (e) {
+    if ($(e.target).val() !== "") {
+      $("#addr-hint-text").addClass("-translate-y-5 text-xs text-sky-500");
+    } else {
+      $("#addr-hint-text").removeClass("-translate-y-5 text-xs text-sky-500");
+    }
+  });
+
+  $("#password").blur(function (e) {
+    if ($(e.target).val() !== "") {
+      $("#password-hint-text").addClass("-translate-y-5 text-xs text-sky-500");
+    } else {
+      $("#password-hint-text").removeClass("-translate-y-5 text-xs text-sky-500");
+    }
+  });
 });
